c194e8689213e9f386993ab8e45b53e359b5bbbb
kalai <kalaiselvib296@gmail.com>
2022-06-08 23:12:29 +0530

    route path

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
+/** @format */
 
-import './App.css';
-import Pages from './Pages';
-import { BrowserRouter } from 'react-router-dom';
-import logo from './assets/img/logo.png';
+import "./App.css";
 
-function App() {
+import logo from "./assets/img/logo.png";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-
+// import Loading from './Loading';
+import Homepage from "./Homepage";
+import ToursList from "./ToursList";
+import TourDetail from "./TourDetail";
+function App({ tours }) {
   return (
-    <div className='page-wrapper'>
-      <BrowserRouter>
-        <img src={logo} className="logo" alt='logo' />
-        <Pages />
-      </BrowserRouter>
+    <div className="page-wrapper">
+      <img src={logo} className="logo" alt="logo" />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/Tourslist" element={<ToursList />} />
+          <Route
+            path="/Tour-Detail/:id"
+            element={<TourDetail tours={tours} />}
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
