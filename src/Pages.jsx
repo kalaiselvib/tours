ea9fcae34c625f3e7b5d850ab0968ed87da5b134
kalai <kalaiselvib296@gmail.com>
2022-06-08 23:00:54 +0530

    route change

--- src/Pages.jsx
+++ src/Pages.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
+/** @format */
 
-import { Route, Routes } from 'react-router-dom';
+import React from "react";
+
+import { Route, Routes } from "react-router-dom";
 
 // import Loading from './Loading';
-import Homepage from './Homepage';
-import TourDetail from './TourDetail';
-import ToursList from './ToursList';
+import Homepage from "./Homepage";
+import TourDetail from "./TourDetail";
+import ToursList from "./ToursList";
 const Pages = ({ tours }) => {
-    return (
-        <Routes>
-            <Route path='/' element={<Homepage />} />
-            <Route path='/Tourslist' element={<ToursList />} />
-            <Route path='/Tour-Detail/:id' element={<TourDetail tours={tours} />} />
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route exact path="/" element={<Homepage />} />
+      <Route path="/Tourslist" element={<ToursList />} />
+      <Route path="/Tour-Detail/:id" element={<TourDetail tours={tours} />} />
+    </Routes>
+  );
+};
 
 export default Pages;
